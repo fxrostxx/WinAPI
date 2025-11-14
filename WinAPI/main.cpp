69b1473209c90d824a58f833e840e9416c9cf207
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-11-15 01:44:09 +0300

    Placeholders DONE

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -3,6 +3,9 @@
 
 //#define MESSAGE_BOX
 
+const char sz_login_placeholder[] = "Enter login";
+const char sz_password_placeholder[] = "Enter password";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lPCmdLine, INT nCmdShow)
@@ -29,8 +32,12 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		//SetFocus(GetDlgItem(hwnd, ID_EDIT_LOGIN));
+		HWND hEditLogin = GetDlgItem(hwnd, ID_EDIT_LOGIN);
+		HWND hEditPassword = GetDlgItem(hwnd, ID_EDIT_PASSWORD);
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)sz_login_placeholder);
+		SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_password_placeholder);
 	}
-		break;
+	break;
 	case WM_COMMAND:	// Обрабатывает комманды с клавиатуры и мыши
 		switch (LOWORD(wParam))
 		{
@@ -43,7 +50,31 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
-			break;
+		break;
+		case ID_EDIT_LOGIN:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, ID_EDIT_LOGIN);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && !strcmp(sz_buffer, sz_login_placeholder))
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			else if (HIWORD(wParam) == EN_KILLFOCUS && !strlen(sz_buffer))
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)sz_login_placeholder);
+		}
+		break;
+		case ID_EDIT_PASSWORD:
+		{
+			HWND hEditPassword = GetDlgItem(hwnd, ID_EDIT_PASSWORD);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditPassword, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && !strcmp(sz_buffer, sz_password_placeholder))
+				SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)"");
+			else if (HIWORD(wParam) == EN_KILLFOCUS && !strlen(sz_buffer))
+				SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_password_placeholder);
+		}
+		break;
 		case IDOK:
 			MessageBox(NULL, "OK button was pressed", "Info", MB_OK | MB_ICONINFORMATION);
 			break;
