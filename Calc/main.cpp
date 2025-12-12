c26d740f59d6f643849e19b3f6238ec210c8ffd5
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-12-12 21:45:10 +0300

    Code structure DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -6,42 +6,11 @@
 #include <stdio.h>
 #include <float.h>
 #include "resource.h"
+#include "dimensions.h"
+#include "ColorsAndSkins.h"
 #ifdef DEBUG
 #include <iostream>
 #endif // DEBUG
-
-
-CONST CHAR g_sz_WINDOW_CLASS[] = "Calc";
-
-CONST INT g_i_BUTTON_SIZE = 50;
-CONST INT g_i_INTERVAL = 5;
-CONST INT g_i_DOUBLE_BUTTON_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
-CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
-CONST INT g_i_DISPLAY_HEIGHT = 22;
-CONST INT g_i_FONT_HEIGHT = g_i_DISPLAY_HEIGHT - 2;
-CONST INT g_i_FONT_WIDTH = g_i_FONT_HEIGHT / 2;
-CONST INT g_i_START_X = 10;
-CONST INT g_i_START_Y = 10;
-CONST INT g_i_BUTTON_START_X = g_i_START_X;
-CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL;
-
-CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_BUTTON_START_X * 2 + 16;
-CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4 + 48;
-
-#define X_BUTTON_POSITION(position) g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
-#define Y_BUTTON_POSITION(position) g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
-
-CONST CHAR g_OPERATIONS[] = "+-*/";
-COLORREF g_BG_COLOR = {};
-CONST INT g_i_WINDOW_COLOR = 0;
-CONST INT g_i_DISPLAY_COLOR = 1;
-CONST INT g_i_FONT_COLOR = 2;
-CONST COLORREF g_clr_COLORS[][3] =
-{
-	{RGB(0, 66, 128), RGB(0, 0, 100), RGB(255, 0, 0)},
-	{RGB(161, 157, 147), RGB(50, 50, 50), RGB(0, 255, 0)}
-};
-CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mistral" };
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
